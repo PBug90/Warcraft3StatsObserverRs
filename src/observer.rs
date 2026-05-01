3d62572f9927ad2af555d7e20476c13ea52bad0d
unknown <pbug90@protonmail.com>
2026-05-01 22:47:33 +0200

    correctly close the handle if it is out of scope

--- src/observer.rs
+++ src/observer.rs
@@ -1,7 +1,10 @@
 use std::marker::PhantomData;
+use std::ops::Deref;
 use winapi::shared::minwindef::LPVOID;
+use winapi::shared::ntdef::HANDLE;
 use winapi::um::errhandlingapi::GetLastError;
-use winapi::um::memoryapi::{FILE_MAP_WRITE, MapViewOfFile, OpenFileMappingW};
+use winapi::um::handleapi::CloseHandle;
+use winapi::um::memoryapi::{FILE_MAP_WRITE, MapViewOfFile, OpenFileMappingW, UnmapViewOfFile};
 
 use std::time::Duration;
 
@@ -83,3 +86,76 @@ impl<'s> ObserverData<'s> {
 // Number generated from SIZE fields of https://github.com/TinkerWorX/Blizzard.Net.Warcraft3
 // noinspection RsAssertEqual
 const _: () = assert!(size_of::<ObserverData>() == 181219642);
+
+/// Owns the Windows handles from `OpenFileMappingW` and `MapViewOfFile`.
+/// Releases them via `Drop` and dereferences to `ObserverData`.
+pub struct ObserverHandle {
+    mapping: HANDLE,
+    view: LPVOID,
+}
+
+unsafe impl Send for ObserverHandle {}
+unsafe impl Sync for ObserverHandle {}
+
+impl ObserverHandle {
+    pub fn new() -> std::io::Result<Self> {
+        Self::new_with_refresh_rate(Duration::from_millis(500))
+    }
+
+    pub fn new_with_refresh_rate(duration: Duration) -> std::io::Result<Self> {
+        let mut path: Vec<u16> = OBSERVER_PATH.encode_utf16().collect();
+        path.push(0);
+
+        let mapping;
+        let errno: i32;
+
+        unsafe {
+            mapping = OpenFileMappingW(FILE_MAP_WRITE, 0, path.as_ptr());
+        }
+
+        if mapping.is_null() {
+            unsafe {
+                errno = GetLastError() as i32;
+            }
+            return Err(std::io::Error::from_raw_os_error(errno));
+        }
+
+        let view: LPVOID;
+
+        unsafe {
+            view = MapViewOfFile(mapping, FILE_MAP_WRITE, 0, 0, 0);
+        }
+
+        if view.is_null() {
+            unsafe {
+                errno = GetLastError() as i32;
+                CloseHandle(mapping);
+            }
+            return Err(std::io::Error::from_raw_os_error(errno));
+        }
+
+        let handle = ObserverHandle { mapping, view };
+        unsafe {
+            let observer = &mut *(view as *mut ObserverData);
+            observer.set_refresh_rate(duration);
+        }
+        Ok(handle)
+    }
+}
+
+impl Deref for ObserverHandle {
+    type Target = ObserverData<'static>;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(self.view as *const ObserverData) }
+    }
+}
+
+impl Drop for ObserverHandle {
+    fn drop(&mut self) {
+        unsafe {
+            UnmapViewOfFile(self.view);
+            CloseHandle(self.mapping);
+        }
+    }
+}
